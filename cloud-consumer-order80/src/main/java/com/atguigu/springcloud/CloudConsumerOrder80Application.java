e8826af6b6a07f4a4a55d22ec25301a073876783
lxj <1549148883@qq.com>
2022-04-20 22:18:15 +0800

    order

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/CloudConsumerOrder80Application.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/CloudConsumerOrder80Application.java
@@ -2,20 +2,18 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-/**
- * @author lixiaolong
- * @date 2020/12/18 16:05
- * @description 支付服务
- */
 @SpringBootApplication
 //@EnableEurekaClient
-//@EnableDiscoveryClient
-public class CloudProviderPayment8001Application {
+//@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class) // 启动该服务时去加载自定义的ribbon配置
+//@RibbonClients
+public class CloudConsumerOrder80Application {
+
     public static void main(String[] args) {
-        SpringApplication.run(CloudProviderPayment8001Application.class, args);
+        SpringApplication.run(CloudConsumerOrder80Application.class, args);
         System.out.println("启动成功");
+
     }
+
 }
--- cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -43,7 +43,6 @@ public class PaymentController {
     @GetMapping("get/{id}")
     public CommonResult<Payment> selectOne(@PathVariable("id") Long id) {
         Payment payment = this.paymentService.queryById(id);
-
         return new CommonResult<Payment>(200, "select success， serverPort：" + serverPort, payment);
     }
 
@@ -51,7 +50,6 @@ public class PaymentController {
     public CommonResult<Payment> create(@RequestBody Payment payment) {
         Payment insert = this.paymentService.insert(payment);
         System.out.println(insert);
-        System.out.println("1234567890");
         return new CommonResult<>(200, "insert success， serverPort：" + serverPort, insert);
     }
 
@@ -59,7 +57,6 @@ public class PaymentController {
     public CommonResult<List<Payment>> queryAllByLimit(@RequestParam(defaultValue = "0") int offset,
                                                        @RequestParam(defaultValue = "10") int limit) {
         List<Payment> payment = this.paymentService.queryAllByLimit(offset, limit);
-
         return new CommonResult<>(200, "select success， serverPort：" + serverPort, payment);
     }
 
@@ -67,12 +64,10 @@ public class PaymentController {
     public Object discovery() {
         List<String> services = discoveryClient.getServices();
         services.forEach(System.out::println);
-
         List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
         instances.forEach(instance -> {
             System.out.println(instance.getServiceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
         });
-
         return this.discoveryClient;
     }
 
