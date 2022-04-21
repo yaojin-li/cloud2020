2afb0f776f2fd87707dce420c96759efef5611f4
lxj <1549148883@qq.com>
2022-04-21 10:34:22 +0800

    eureka

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/CloudConsumerOrder80Application.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/CloudConsumerOrder80Application.java
@@ -2,17 +2,19 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
+@EnableDiscoveryClient
 //@RibbonClient(name = "CLOUD-PAYMENT-SERVICE", configuration = MySelfRule.class) // 启动该服务时去加载自定义的ribbon配置
 //@RibbonClients
 public class CloudConsumerOrder80Application {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudConsumerOrder80Application.class, args);
-        System.out.println("启动成功");
+        System.out.println("CloudConsumerOrder80Application 启动成功");
 
     }
 
